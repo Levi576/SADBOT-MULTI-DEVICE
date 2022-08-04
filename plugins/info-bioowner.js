a96e7169d0c569aeec5a6cb825a0dab3d14e4042
Levi <110154273+Levi576@users.noreply.github.com>
2022-08-04 17:25:30 +0800

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -2,23 +2,23 @@ let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
 *💌 Nama* : Sad-Bot
-*✉️ Nama RL* : Fahri
+*✉️ Nama RL* : levi
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
+*⏰ Tanggal lahir* : 7 desember 2007
+*🎨 Umur* : 14
+*🧮 Kelas* : 7
+*🧩 Hobby* : Nonton Hanime, Chatting, suka nonton hentai
+*💬 Sifat* : pendiam, satir, Kanjut, Prik, Pedofil
+*🗺️ Tinggal* : Indo, berau, kaltim, suaran
 *❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
 *💔 Benci* : autis, anak epep, seleb
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+*📷 instagran* : private
+*🇫  Facebook* : levi adnansyah
+*🏮 Chanel Youtube* : private
+*🐈 Github:* Levi576
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
